bc5384a709f955c509f934733664b44759a050fa
krystianl <krystian.leszczewski@nask.pl>
2018-03-08 13:51:05 +0100

    Created Config

--- src/main/java/com/example/jmsspring/resource/ProducerResource.java
+++ src/main/java/com/example/jmsspring/resource/ProducerResource.java
@@ -1,4 +1,28 @@
 package com.example.jmsspring.resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Queue;
+
+@RestController
+@RequestMapping("/rest/publish")
 public class ProducerResource {
+
+    @Autowired
+    Queue queue;
+
+    @Autowired
+    JmsTemplate jmsTemplate;
+
+    @GetMapping("/{message}")
+    public String publish(@PathVariable("message") final String message){
+
+
+        return message;
+    }
 }
